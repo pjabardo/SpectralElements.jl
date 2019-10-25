a590818783fe7c63d1fd57a174d5b951ec54a396
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2019-10-25 15:38:28 -0300

    The basics for reading old Nekton files is there
    The mesh part really, really sucks!

--- src/SpectralElements.jl
+++ src/SpectralElements.jl
@@ -5,7 +5,7 @@ module SpectralElements
 module Mesh
 include("mesh1d.jl")
 include("mesh2d.jl")
-#include("nektar.jl")
+include("nektar.jl")
 
 end
 
--- src/nektar.jl
+++ src/nektar.jl
@@ -5,7 +5,7 @@ function section(flines, header)
 
     nl = length(flines)
     for i in 1:nl
-        if length(search(flines[i], header)) > 0
+        if occursin(header, flines[i])
             return i
         end
     end
@@ -15,14 +15,15 @@ end
     
 
 function element(rea, idx)
-    if length(search(uppercase(rea[1]), "QUAD")) > 0
-        el = Quad()
+    if occursin("QUAD", uppercase(rea[1]))
+        el = Quad
     else
         throw(ArgumentException("Only Quad type elements are possible"))
     end
-    xverts = [parse(Float64, x) for x = split(strip(rea[2]))][1:nverts(el)]
-    yverts = [parse(Float64, x) for x = split(strip(rea[2]))][1:nverts(el)]
-    neigh = [(0,0) for i in 1:nedges(Quad)]
+    nv = nverts(el)
+    xverts = [parse(Float64, x) for x = split(strip(rea[2]))][1:nv]
+    yverts = [parse(Float64, x) for x = split(strip(rea[2]))][1:nv]
+    neigh = [(0,0) for i in 1:nedges(el)]
 
     Quad{Float64}(idx, xverts, yverts, neigh)
     
@@ -33,7 +34,7 @@ function mesh(rea)
 
     isec = section(rea, "MESH DATA") + 1
     nelem = parse(Int, split(strip(rea[isec]), " ")[1])
-    elems = Array(Quad{Float64}, nelem)
+    elems = Vector{Quad{Float64}}(undef, nelem)
     isec += 1
     for e = 1:nelem
         elems[e] = element(rea[isec:(isec+2)], e)
@@ -49,26 +50,26 @@ end
 function bndry_sec(rea, isec, nterms=2)
 
     s = split(strip(rea[isec]))
-    tag = strip(s[1])
+    tag = strip(s[1])[1]
     nums = [parse(Float64, strip(ss)) for ss in s[2:end]]
     el = Int(round(nums[1]))
     ed = Int(round(nums[2]))
     p = nums[3:end]
-    if islower(tag)
+    if islowercase(tag)
         funs = rea[(isec+1):(isec+nterms)]
         isec += nterms + 1
     else
         funs = String[]
         isec += 1
     end
-    return mshBndry(tag, el, ed, p, funs), isec
+    return mshBndry(string(tag), el, ed, p, funs), isec
 end
 
 function bndry_conds(rea, elems)
     isec = section(rea, "BOUNDARY CONDITIONS")+2
     bndrylst = mshBndry{Float64}[]
     for el in elems
-        ne = Mesh2d.nedges(el)
+        ne = nedges(el)
         for edge = 1:ne
             b, isec = bndry_sec(rea, isec)
             if b.tag == 'E' || b.tag == 'P'
@@ -84,14 +85,14 @@ function bndry_conds(rea, elems)
     nb = length(bndrylst)
     for i = 1:nb
         el = bndrylst[i].elem
-        ed = bndrylst[i].edge
+        ed = bndrylst[i].face
         elems[el].neigh[ed] =(-1, -i)
     end
     return bndrylst
 end
     
 
-type NekCurve 
+struct NekCurve 
     cname::String
     line::String
 end
